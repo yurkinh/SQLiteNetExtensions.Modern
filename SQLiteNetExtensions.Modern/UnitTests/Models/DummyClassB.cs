c6b17a52bd7d757c11000e5e2739743404a1748d
MykhailoDav <davudenko.mukhailo@gmail.com>
2024-12-13 20:26:30 +0200

    added UnitTests

---
+++ SQLiteNetExtensions.Modern/UnitTests/Models/DummyClassB.cs
@@ -0,0 +1,14 @@
+using SQLiteNetExtensions.Attributes;
+
+namespace SQLiteNetExtensions.UnitTests.Models
+{
+    public class DummyClassB
+    {
+        [OneToOne]
+        public DummyClassA OneA { get; set; }
+
+        [OneToOne]
+        public DummyClassC ObjectC { get; set; }
+        public int DummyClassCKey { get; set; }
+    }
+}
