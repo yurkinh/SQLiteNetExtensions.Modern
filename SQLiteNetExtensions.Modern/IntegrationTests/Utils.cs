633ceebfea2e4b3071728bec830d9b3dd2f38e6e
MykhailoDav <davudenko.mukhailo@gmail.com>
2024-12-16 17:47:29 +0200

    added integration tests

---
+++ SQLiteNetExtensions.Modern/IntegrationTests/Utils.cs
@@ -0,0 +1,32 @@
+using Microsoft.Maui.Storage;
+using SQLite;
+using SQLitePCL;
+
+namespace SQLiteNetExtensions.IntegrationTests;
+
+public class Utils
+{
+    static Utils()
+    {
+        Batteries.Init();
+    }
+
+    public static string DatabaseFilePath => Path.Combine(FileSystem.AppDataDirectory, "database.db3");
+
+    public static SQLiteConnection CreateConnection()
+    {
+        var con = new SQLiteConnection(DatabaseFilePath);
+        raw.sqlite3_trace(con.Handle, Log, null);
+        return con;
+    }
+
+    private static void Log(object userData, string statement)
+    {
+        Console.WriteLine(statement);
+    }
+
+    public static SQLiteAsyncConnection CreateAsyncConnection()
+    {
+        return new SQLiteAsyncConnection(DatabaseFilePath);
+    }
+}
