f90d933aeca92722a4797c028af45e7418faf1fe
MykhailoDav <davudenko.mukhailo@gmail.com>
2024-12-17 16:02:10 +0200

    downgraded .NET version

--- SQLiteNetExtensions.Modern/IntegratedTestsSampleApp/App.xaml.cs
+++ SQLiteNetExtensions.Modern/IntegratedTestsSampleApp/App.xaml.cs
@@ -5,10 +5,6 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-	}
-
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new AppShell());
+		MainPage = new AppShell();
 	}
 }
